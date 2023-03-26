35c90a699e6955172c115b55d1af4987975f5350
laubjkri <johndoe@example.com>
2023-03-26 20:29:48 +0200

    lala

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  
+  // The selector searches the DOM for an element with the name of the selector, and then injects the component here.
+  selector: "app-root",
+  // template: `
+  // <h1>My App</h1>
+  // <p>Keeping track of the media I want to watch.</p>
+  // `
+  templateUrl: "./app.component.html",
+  // styles: [`
+  //   h1 {color: #ffffff}
+  //   .description {
+  //     font-style: italic;
+  //     color: green;      
+  //   }  
+  // `],
+  styleUrls: ["./app.component.css"]
+  
 })
-export class AppComponent {
-  title = 'angular-essential-training';
+export class AppComponent{
+  firstMediaItem = {
+    id: 1,
+    name: "Firebug",
+    medium: "Series",
+    category: "Science Fiction",
+    year: 2010,
+    watchedOn: 1294166565384,
+    isFavorite: false
+  }
+  
+  onMediaItemDelete(mediaItem: any) {
+    
+  }
 }
---
+++ src/app/media-item.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+
+@Component({
+  selector: "mw-media-item",
+  templateUrl: "./media-item.component.html",
+  styleUrls: ["./media-item.component.css"]
+})
+export class MediaItemComponent { 
+  @Input() mediaItem: any;
+  @Output() delete = new EventEmitter();
+  
+  onDelete() {
+    console.log("Deleted");
+    this.delete.emit(this.mediaItem);
+  }
+}
